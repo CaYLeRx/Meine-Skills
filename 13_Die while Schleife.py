29f5fc1cc1dc8da401520a8675c2c98e07ef425e
CaYLeR_x <115471219+CaYLeRx@users.noreply.github.com>
2022-10-10 18:07:09 +0200

    Add files via upload

---
+++ 13_Die while Schleife.py
@@ -0,0 +1,11 @@
+# Ausführung endlos
+while 5 < 10:
+    print("Hier steht ein Code, der wiederholt ausheführt werden soll")
+
+
+# Ausführung begrenzt
+counter = 5
+while counter < 10:
+    print("Hier steht ein Code, der '5mal' wiederholt ausheführt werden soll")
+    counter += 1
+
