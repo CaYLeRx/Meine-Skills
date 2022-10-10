96af3a2fd99fa6cc28f110bd04140e6699b7240e
CaYLeR_x <115471219+CaYLeRx@users.noreply.github.com>
2022-10-10 18:27:44 +0200

    Add files via upload

---
+++ 14_Was sind Listen.py
@@ -0,0 +1,18 @@
+
+test_variable = 10
+
+#number = [x]/ number(s) = [x,x,x]
+numbers = [3,9,14,18,20]
+number = [7]
+
+print(list(numbers))
+print(list(number))
+
+#name = ["xxx"] / names = ["xxx", "xxx", "xxx"]
+names = ["Thomas", "Phil", "Mike", "Marco"]
+name = ["Vincent"]
+
+print(list(names))
+print(list(name))
+
+
