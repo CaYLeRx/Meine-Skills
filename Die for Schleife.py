73e358c5a6966e858cba8b8afa4bd15d4f544222
CaYLeR_x <115471219+CaYLeRx@users.noreply.github.com>
2022-10-10 19:13:15 +0200

    Update and rename 16_Die for Schleife.py to Die for Schleife.py

--- Die for Schleife.py
+++ Die for Schleife.py
@@ -5,3 +5,5 @@ for element in [1,2,3,4]:
 for element in "Ich werde buchstabiert weil ich ein String bin":
     print(element)
 
+for element in range(5, 50, 5): #(5, Y, Z = ab 5 bis Z // #(X, Y, 5 = sprünge
+    print(element)
