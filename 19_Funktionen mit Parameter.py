96cd39debcfa629ee7d9be153baf89ad440b212c
CaYLeR_x <115471219+CaYLeRx@users.noreply.github.com>
2022-10-10 19:41:32 +0200

    Add files via upload

---
+++ 19_Funktionen mit Parameter.py
@@ -0,0 +1,42 @@
+# Long
+def say_hello1():
+    print("Hallo Mike")
+    print("Willkommen zurück")
+
+
+def say_hello2():
+    print("Hallo Vincent")
+    print("Willkommen zurück")
+
+def say_hello3():
+    print("Hallo Marco")
+    print("Willkommen zurück")
+
+say_hello1()
+say_hello2()
+say_hello3()
+print("Das war die 'long' Option 'ENDE' ")
+
+#short
+
+def say_hello(name):
+    print("Hallo " + name)
+    print("Willkommen zurück")
+
+say_hello("Mike")
+say_hello("Vincent")
+say_hello("Marco")
+print("Das war die 'short' Option 'ENDE' ")
+
+#Vor- Nachname
+
+def say_hello(first_name, last_name):
+    print("Hallo " + first_name + " " + last_name)
+    print("Willkommen zurück")
+
+say_hello("Mike" , "Apexmann")
+say_hello("Vincent", "noob")
+say_hello("Marco" , "WoWguy")
+print("Das war die 'short' Option 'ENDE' ")
+
+
