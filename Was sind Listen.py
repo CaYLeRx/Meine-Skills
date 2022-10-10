50a7fbf85a540256d59fa69b1b85d93a426633e0
CaYLeR_x <115471219+CaYLeRx@users.noreply.github.com>
2022-10-10 18:42:35 +0200

    Update Was sind Listen.py

--- Was sind Listen.py
+++ Was sind Listen.py
@@ -15,4 +15,10 @@ name = ["Vincent"]
 print(list(names))
 print(list(name))
 
+#           0        1       2        3
+#          -4       -3      -2       -1
+names = ["Thomas", "Phil", "Mike", "Marco"]
+print(names)
 
+names[0] = "Vincent" # Name ersetzen
+print(names)
