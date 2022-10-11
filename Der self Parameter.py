b7bbfc3b9d427949e647b32f462e64f4426e2d87
CaYLeR_x <115471219+CaYLeRx@users.noreply.github.com>
2022-10-11 19:29:50 +0200

    Update Der self Parameter.py

--- Der self Parameter.py
+++ Der self Parameter.py
@@ -4,16 +4,25 @@ class Auto:
         self.auto_marke = None
         self.power_score = None
         self.farbe = None
+   
+    def drive(self, x, y):
+        self.x_position += x
+        self.y_position -= y
 
-Auto1 = Auto() #Auto Objekt 1
-print(Auto1.auto_marke)
-Auto1.auto_marke = "Porsche"
-Auto1.power_score = 460
-Auto1.farbe = "Red"
 
+Auto1 = Auto()  #Auto Objekt 1
+print(Auto1.x_position)
+print(Auto1.y_position)
+Auto1.drive(5, 2)
+print(Auto1.x_position)
+print(Auto1.y_position)
+
+
+        
 print(Auto1.auto_marke)
 print(Auto1.power_score)
 print(Auto1.farbe)
+
 
 Auto2 = Auto() #Auto Objekt 2
 print(Auto2.auto_marke)
