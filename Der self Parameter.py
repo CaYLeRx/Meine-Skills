7551b0aadbfa7ed2d95d320c579808e58ac10421
CaYLeR_x <115471219+CaYLeRx@users.noreply.github.com>
2022-10-20 20:03:00 +0200

    Update Der self Parameter.py

--- Der self Parameter.py
+++ Der self Parameter.py
@@ -10,7 +10,7 @@ class Auto:
         self.y_position -= y
 
 
-Auto1 = Auto()  #Auto Objekt 1
+Auto1 = Auto()  # Auto Objekt 1
 print(Auto1.x_position)
 print(Auto1.y_position)
 Auto1.drive(5, 2)
@@ -24,16 +24,16 @@ print(Auto1.power_score)
 print(Auto1.farbe)
 
 
-Auto2 = Auto() #Auto Objekt 2
+Auto2 = Auto()  # Auto Objekt 2
 print(Auto2.auto_marke)
 Auto2.auto_marke = "BMW"
 Auto2.power_score = 210
 Auto2.farbe = "Black"
 
-Auto3 = Auto1 #das Objekt greift auf das selbe Element zu
+Auto3 = Auto1  # das Objekt greift auf das selbe Element zu
 
 print(Auto1)
 print(Auto2)
 print(Auto3)
 print(Auto1.power_score) 
-print(Auto3.power_score) #referenz bleibt gleich
+print(Auto3.power_score)  # referenz bleibt gleich
